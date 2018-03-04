13601ba29d6af58b58399220cb8ee2b3edb9744f
Michael Mühlebach <michael@anduin.ch>
2018-03-04 15:53:04 +0100

    Add tests for name generation

---
+++ test/timepiece/TimeNamesGermanTest.java
@@ -0,0 +1,30 @@
+package timepiece;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeNamesGermanTest {
+
+    @Test
+    void getTimeStrings(TestReporter reporter) {
+        List<String>[][] timeStrings = TimeNamesGerman.getTimeStrings();
+
+        assertEquals(12, timeStrings.length);
+        assertTrue(Arrays.stream(timeStrings).allMatch((arr) -> arr.length == 12));
+
+        reporter.publishEntry("first dimension", String.valueOf(timeStrings.length));
+        reporter.publishEntry("second dimension", Arrays.stream(timeStrings).map((arr) -> String.valueOf(arr.length)).reduce("", (a, b) -> a + ", " + b));
+
+        for (int i = 0; i < timeStrings.length; i++) {
+            for (int j = 0; j < timeStrings[i].length; j++) {
+                reporter.publishEntry("(" + i + "," + j + ")", String.valueOf(timeStrings[i][j]));
+            }
+        }
+    }
+
+}
